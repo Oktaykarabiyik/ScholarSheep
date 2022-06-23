04a88e7b7adddccfb9429088cebd1c06c1eece71
betulturkselci <you@example.com>
2022-06-23 15:22:08 +0300

    login and register pages updated

---
+++ screens/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path("", views.index),
+    path("index", views.index),
+    path("ogrencigiris", views.ogrencigiris),
+    path("ogrencikayit", views.ogrencikayit),
+    path("bursverenkayit", views.bursverenkayit),
+    path("bursverengiris", views.bursverengiris),
+]
---
+++ screens/views.py
@@ -0,0 +1,22 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+
+def ogrencigiris(request):
+    return render(request,"ogrencigiris.html")
+
+def bursverengiris(request):
+    return render(request,"bursverengiris.html")
+
+def ogrencikayit(request):
+    return render(request,"ogrencikayit.html")
+
+def bursverenkayit(request):
+    return render(request,"bursverenkayit.html")
+
+
