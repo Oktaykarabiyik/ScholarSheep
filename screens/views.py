6d5ed83fbf1e5621f6e6ca61ba2c95ff68e3d86f
Oktaykarabiyik <77680642+Oktaykarabiyik@users.noreply.github.com>
2022-06-29 17:19:45 +0300

    Oktaykarabiyik

--- screens/views.py
+++ screens/views.py
@@ -1,8 +1,28 @@
-
+from distutils.command.config import config
 from django.http import HttpResponse
 from django.shortcuts import render
+import pyrebase
 
-# Create your views here.
+
+config={
+  "apiKey": "AIzaSyBmM91bv9cLoRFOYHO-0k9jBv9aN3Lhh8o",
+  "authDomain": "scholarsheep-8fbec.firebaseapp.com",
+  "projectId": "scholarsheep-8fbec",
+  "databaseURL": "https://scholarsheep-8fbec-default-rtdb.firebaseio.com",
+  "storageBucket": "scholarsheep-8fbec.appspot.com",
+  "messagingSenderId": "177314107895",
+  "appId": "1:177314107895:web:4e7164bbb31e683d38def2",
+  "measurementId": "G-ETXWFENSN7"
+}
+
+firebase=pyrebase.initialize_app(config)
+authe=firebase.auth()
+database=firebase.database()
+
+name=database.child('Ogrenciler').child('e_mail').get().val()
+print(name)
+
+
 
 def index(request):
     return render(request,"index.html")
