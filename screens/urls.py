3052a525b2074433108eac708676241cbb3a40e4
Oktaykarabiyik <77680642+Oktaykarabiyik@users.noreply.github.com>
2022-06-30 11:27:03 +0300

    Oktaykarabiyik

--- screens/urls.py
+++ screens/urls.py
@@ -9,13 +9,11 @@ urlpatterns=[
     path("ogrencikayit", views.ogrencikayit),
     path("bursverenkayit", views.bursverenkayit),
     path("bursverengiris", views.bursverengiris),
-<<<<<<< HEAD
     path("ogrenciform",views.ogrenciform),
     path("profilim",views.ogrenciprofil),
-=======
     path("profilim", views.ogrenciprofil),
     path("bursverenprofil", views.bursverenprofil),
     path("ilanlar", views.ilanlar),
     path("ilanekle", views.ilanekle),
->>>>>>> 06de4e2c68f2972a1e44b9514983e02f9043025b
+
 ]
