f5f828a637d53ab7ba39ca5b17f1bd7ee6d604a0
Oktaykarabiyik <77680642+Oktaykarabiyik@users.noreply.github.com>
2022-07-05 15:08:13 +0300

    ufak değişiklikler

--- screens/views.py
+++ screens/views.py
@@ -61,6 +61,46 @@ def postsignUp(request):
      return render(request,"ogrencigiris.html")
 
 
+def bursverensignIn(request):
+    return render(request,"index.html")
+ 
+def bursverenpostsignIn(request):
+    email=request.POST.get('email')
+    pasw=request.POST.get('pass')
+    try:
+        # if there is no error then signin the user with given email and password
+        user=authe.sign_in_with_email_and_password(email,pasw)
+        return render(request,"index.html",{"email":email})
+    except:
+        message="Invalid Credentials!!Please ChecK your Data"
+        return render(request,"bursverengiris.html",{"message":message})
+    session_id=user['idToken']
+    request.session['uid']=str(session_id)
+    
+ 
+def bursverencikis(request):
+    try:
+        del request.session['uid']
+    except:
+        pass
+    return render(request,"bursverengiris.html")
+ 
+ 
+def bursverenpostsignUp(request):
+     email = request.POST.get('email')
+     passs = request.POST.get('pass')
+     name = request.POST.get('name')
+     try:
+        # creating a user with the given email and password
+        user=authe.create_user_with_email_and_password(email,passs)
+        uid = user['localId']
+        idtoken = request.session['uid']
+        print(uid)
+     except:
+        return render(request, "bursverenkayit.html")
+     return render(request,"bursverengiris.html")
+
+
 def postcreate(request):
 
     adsoyad=request.POST.get('adsoyad')
